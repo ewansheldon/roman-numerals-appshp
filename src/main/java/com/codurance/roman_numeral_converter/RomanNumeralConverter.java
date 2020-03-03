34e0b2bf9ebc111ff22b7cbbcdeb1058549104b3
Ewan <ewan.s@cucumberwifi.io>
2020-03-03 11:38:45 +0000

    refactor roman numerals for 1,2,3

--- src/main/java/com/codurance/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/com/codurance/roman_numeral_converter/RomanNumeralConverter.java
@@ -2,8 +2,12 @@ package com.codurance.roman_numeral_converter;
 
 public class RomanNumeralConverter {
     public String arabicToRoman(int arabic) {
-        if (arabic == 3) return "III";
-        if (arabic == 2) return "II";
-        return "I";
+        String roman = "";
+
+        for (int i = 0; i < arabic; i++) {
+            roman += "I";
+        }
+
+        return roman;
     }
 }
